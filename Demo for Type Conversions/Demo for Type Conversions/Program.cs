7f0c00c68e0fce68f32bb6da520f5e4b30c215e9
Israel Mesa <mesa.israel50@gmail.com>
2017-12-02 11:01:17 -0500

    Type Conversions

--- Demo for Type Conversions/Demo for Type Conversions/Program.cs
+++ Demo for Type Conversions/Demo for Type Conversions/Program.cs
@@ -10,6 +10,45 @@ namespace Demo_for_Type_Conversions
     {
         static void Main(string[] args)
         {
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            //int i = 6;
+            int i = 1000; 
+            //byte b = i;
+            byte b = (byte)i; // bits will be lost and console will print 232
+            Console.WriteLine(b);
+            //Cannot implicitly convert type 'int' to 'byte'. An explicit conversion exists (are you missing a cast?)
+
+            //string number = "1234";
+            //int a = Convert.ToInt32(number);
+            //Console.WriteLine(a);
+
+            try
+            {
+                var number = "1234";
+                byte a = Convert.ToByte(number);
+                Console.WriteLine(a);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("number could not be converted to a byte.");
+            }
+
+            try
+            {
+                string str = "true";
+                bool c = Convert.ToBoolean(str);
+                Console.WriteLine(c);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("number could not be converted to a byte.");
+            }
+
         }
     }
 }
