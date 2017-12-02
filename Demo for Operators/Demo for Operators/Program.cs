ea7ae265aaa54c1d098e4af8f9f03f55fdb8a249
Israel Mesa <mesa.israel50@gmail.com>
2017-12-02 12:44:31 -0500

    Operators

---
+++ Demo for Operators/Demo for Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_for_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a / b); //both are integers
+            Console.WriteLine((float)a / (float)b);
+
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+
+            var x = 1;
+            var y = 2;
+            var z = 3;
+            Console.WriteLine(z > x && z > y);
+            Console.WriteLine(z > x && z == y);
+            Console.WriteLine(z > x || z == y);
+
+        }
+    }
+}
