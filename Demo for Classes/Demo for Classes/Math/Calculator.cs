2100922cabfe6743432db9c5b8de7a6829625af1
Israel Mesa <mesa.israel50@gmail.com>
2017-12-03 16:07:06 -0500

    Classes

---
+++ Demo for Classes/Demo for Classes/Math/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_for_Classes.Math
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Demo for Classes/Demo for Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo_for_Classes.Math;
+
+namespace Demo_for_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.add(1, 2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
