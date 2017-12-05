80918f5a4d30ff0999fe7da62b3fe262eef6d7bd
Israel Mesa <mesa.israel50@gmail.com>
2017-12-04 23:32:22 -0500

    Demo 2

---
+++ Demo for Reference Types and Value Types 2/Demo for Reference Types and Value Types 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_for_Reference_Types_and_Value_Types_2
+{
+    public class person
+    {
+        public int Age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new person {Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment (int number)
+        {
+            number += 10;   
+        }
+        
+        public static void MakeOld(person person)
+        {
+            person.Age += 10;
+        }
+    }
+}
