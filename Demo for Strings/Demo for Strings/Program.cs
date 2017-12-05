e4f48aa42e04da565945cecb003be127ec46bdfe
Israel Mesa <mesa.israel50@gmail.com>
2017-12-04 19:52:50 -0500

    Strings

---
+++ Demo for Strings/Demo for Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_for_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Israel";
+            var lastName = "Mesa";
+
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format("{0} {1}", firstName, lastName);
+            Console.WriteLine(myFullName);
+
+            var names = new string[3] { "Juan", "Jose", "Maria" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi Juan
+                        Look into the following paths
+                        c:\folder1\folder2
+                        c:\folder3\folder4";
+            Console.WriteLine(text);
+        }
+    }
+}
