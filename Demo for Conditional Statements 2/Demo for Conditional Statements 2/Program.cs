5b71fff77ecc151bf96f0d355af377448d4ede8e
Israel Mesa <mesa.israel50@gmail.com>
2017-12-06 13:57:04 -0500

    Demo 2

---
+++ Demo for Conditional Statements 2/Demo for Conditional Statements 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_for_Conditional_Statements_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch(season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's Autumn");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's Summer");
+                    break;
+                case Season.Spring:
+                    Console.WriteLine("It's Spring");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("It's Winter");
+                    break;
+                default:
+                    Console.WriteLine("Huh? What are you talking about?");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Demo for Conditional Statements 2/Demo for Conditional Statements 2/Season2.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Demo_for_Conditional_Statements_2
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
