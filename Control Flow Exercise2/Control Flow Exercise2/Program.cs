7b1a075863e6b20cb7ba05e74582c803bc200332
Israel Mesa <mesa.israel50@gmail.com>
2017-12-06 14:20:16 -0500

    Exercise 2

---
+++ Control Flow Exercise2/Control Flow Exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Flow_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > a)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
