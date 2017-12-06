63dfb9ad6d4536d9464196acd29760d215a1d418
Israel Mesa <mesa.israel50@gmail.com>
2017-12-06 16:32:42 -0500

    While Loop

---
+++ Demo for While Loops/Demo for While Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_for_While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (var i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //        i++;
+            //    }
+            //}
+
+            while (true)
+            {
+                Console.WriteLine("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
