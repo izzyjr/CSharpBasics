20cce2c5607ae71eefb33c96941659e63a09e0af
Israel Mesa <mesa.israel50@gmail.com>
2017-12-06 16:51:58 -0500

    Random Class

---
+++ Random/Random/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            const int passwordlength = 10;
+
+            var buffer = new char[passwordlength];
+            for (var i = 0; i < passwordlength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
