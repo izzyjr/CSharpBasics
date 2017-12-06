23598d06edb1f1e4ff234e4432a6112ae7a7bae0
Israel Mesa <mesa.israel50@gmail.com>
2017-12-06 14:27:54 -0500

    Exercise 3

---
+++ Control Flow Exercise3/Control Flow Exercise3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Flow_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            if (height > width)
+            {
+                Console.WriteLine("Portrait");
+            }
+            else if (width > height)
+            {
+                Console.WriteLine("Landscape");
+            }
+        }
+    }
+}
