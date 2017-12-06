8a107822393df386129bb6064636efad2c08a66a
Israel Mesa <mesa.israel50@gmail.com>
2017-12-06 14:15:53 -0500

    Exercise 1

---
+++ Control Flow Exercise1/Control Flow Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Flow_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter number between 1 and 10: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 10 && number > 1)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
