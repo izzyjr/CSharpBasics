5ac19263142403ef5311e650c3557932462b3b11
Israel Mesa <mesa.israel50@gmail.com>
2017-12-14 00:09:22 -0500

    Exercise 2

--- Exercisese for Working with Text/Exercisese for Working with Text/Program.cs
+++ Exercisese for Working with Text/Exercisese for Working with Text/Program.cs
@@ -11,31 +11,65 @@ namespace Exercisese_for_Working_with_Text
         static void Main(string[] args)
         {
             //Exercise1
-            Console.WriteLine("Enter a few numbers separated by a hyphen: ");
+
+            //Console.WriteLine("Enter a few numbers separated by a hyphen: ");
+            //var input = Console.ReadLine();
+            //var changedInput = input.Split('-');
+            //var count = 0;
+
+            //for (var i = 0; i < changedInput.Length; i++)
+            //{
+            //    if (i != (changedInput.Length - 1))
+            //    {
+            //        if (Convert.ToInt32(changedInput[i]) < Convert.ToInt32(changedInput[i + 1]))
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //}
+            //if ((changedInput.Length - 1) == count)
+            //{
+            //    Console.WriteLine("Consecutive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Consecutive");
+            //}
+
+            //Exercise2
+
+            Console.WriteLine("Eenter a few numbers separated by a hyphen: ");
             var input = Console.ReadLine();
             var changedInput = input.Split('-');
             var count = 0;
 
-            for (var i = 0; i < changedInput.Length; i++)
+            if (String.IsNullOrEmpty(input))
             {
-                if (i != (changedInput.Length - 1))
-                {
-                    if (Convert.ToInt32(changedInput[i]) < Convert.ToInt32(changedInput[i + 1]))
-                    {
-                        count++;
-                    }
-                }
-            }
-            if ((changedInput.Length - 1) == count)
-            {
-                Console.WriteLine("Consecutive");
+                Console.WriteLine("Exited program");
             }
             else
             {
-                Console.WriteLine("Not Consecutive");
+                for (var i = 0; i < changedInput.Length; i++)
+                {
+                    for (var j = 0; j < changedInput.Length; j++)
+                    {
+                        if (Convert.ToInt32(changedInput[i]) == Convert.ToInt32(changedInput[j]))
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count > changedInput.Length)
+                {
+                    Console.WriteLine("Duplicate");
+                    //Console.WriteLine(count);
+                }
+                else
+                {
+                    Console.WriteLine("No Duplicates");
+                }
             }
 
-            //Exercise2
 
 
         }
